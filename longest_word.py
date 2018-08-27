e36610eba2b4d3d284b0612a0b1a5b2687243fce
Winshen <winshen@uchicago.edu>
2018-08-27 16:04:55 -0500

    fixed bug so smallest lexicographic order is used when list d word lengths are equal

--- longest_word.py
+++ longest_word.py
@@ -1,14 +1,18 @@
+import pdb
+
 def longest_word(s, d):
     lengths = [(entry, len(entry)) for entry in d]
-    compare_d = sorted(lengths, key=lambda x: x[1], reverse = True)
+    sorted_d = sorted(lengths, key = lambda x: (-x[1], x[0]))
 
-    for word, length in compare_d:
+    for word, length in sorted_d:
         j = 0
         for i in range(0, len(s)):
-            if word[j] == s[i]:
+            if j < len(word) and word[j] == s[i]:
                 j += 1
-            if j == len(word)-1:
+            if j == len(word):
                 return word
     return ''
 
+print(longest_word("abpcplea", ["a", "b", "c"]))
+print(longest_word("abpcplea", ["ba", "ab", "a", "b"]))
 print(longest_word('abpcplea', ["ale","apple","monkey","plea"]))
